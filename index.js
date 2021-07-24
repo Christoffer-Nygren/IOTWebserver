000ccbc0439c4135d0b884ea1da8c6539e6af180
CrawlingMuffin <c_nygren@protonmail.com>
2021-07-24 15:31:14 +0200

    Got notifications working

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require('express')
+var axios = require('axios');
+var FormData = require('form-data');
 const app = express()
 const port = 3000
 
+const sendNotification = (stringToSend) => {
+  var data = new FormData();
+  data.append('To', process.env.TWILIO_TO);
+  data.append('From', process.env.TWILIO_FROM);
+  data.append('Body', 'test');
+
+  var config = {
+    method: 'post',
+    url: process.env.TWILIO_URL,
+    headers: {
+      'Authorization': process.env.TWILIO_AUTH_TOKEN,
+      ...data.getHeaders()
+    },
+    data : data
+  };
+
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+}
+
 app.get('/', (req, res) => {
+  sendNotification('Test')
   res.send('Notification handler')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
